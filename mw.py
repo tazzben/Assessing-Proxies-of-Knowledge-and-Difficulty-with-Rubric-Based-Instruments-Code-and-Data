5b0438fbfc2346caaa477a0b78beaf283839c190
Ben Smith <ben@bensresearch.com>
2022-11-20 06:29:45 -0600

    Remove toList in mw

--- mw.py
+++ mw.py
@@ -1,25 +1,24 @@
 import pandas as pd
 from scipy.stats import mannwhitneyu, ttest_rel
 
-
 def main():
     data = pd.read_csv("StudentsProjects14.csv")
     project4 = data[data['Variable'].astype(str).str[-2:] == "_4"]
     project1 = data[data['Variable'].astype(str).str[-2:] != "_4"]
-    U1, p = mannwhitneyu(project1["Average Logistic"].to_list(), project4["Average Logistic"].to_list())
+    U1, p = mannwhitneyu(project1["Average Logistic"], project4["Average Logistic"])
     print("MW Test")
     print(U1)
     print(p)
     print("TTEST")
-    t, p = ttest_rel(project1["Average Logistic"].to_list(), project4["Average Logistic"].to_list())
+    t, p = ttest_rel(project1["Average Logistic"], project4["Average Logistic"])
     print(t)
     print(p)
-    U1, p = mannwhitneyu(project1["Average Marginal Logistic"].to_list(), project4["Average Marginal Logistic"].to_list())
+    U1, p = mannwhitneyu(project1["Average Marginal Logistic"], project4["Average Marginal Logistic"])
     print("MW Test Marginal")
     print(U1)
     print(p)
     print("TTEST Marginal")
-    t, p = ttest_rel(project1["Average Marginal Logistic"].to_list(), project4["Average Marginal Logistic"].to_list())
+    t, p = ttest_rel(project1["Average Marginal Logistic"], project4["Average Marginal Logistic"])
     print(t)
     print(p)
     print("Marginal Means")
