199a14cd4f94c4e15995fc18e6918dd867de570a
Ben Smith <ben@bensresearch.com>
2023-01-03 06:45:31 -0600

    Add discrete calculations

--- KDEMarginal.py
+++ KDEMarginal.py
@@ -27,12 +27,13 @@ def ManageKDEProcess(bandwidth, data):
     xinterval, yprojections = RunKDE(bandwidth, data)
     return xinterval, yprojections
 
-def CreateCompareKDEPlots():
+def CreateCompareKDEPlots(discrete=False):
+    field = "Average Discrete Marginal Logistic" if discrete else "Average Marginal Logistic"
     data = pd.read_csv("StudentsProjects14.csv")
     project4 = data[data['Variable'].astype(str).str[-2:] == "_4"]
     project1 = data[data['Variable'].astype(str).str[-2:] != "_4"]
-    bandwidths = [2.5*Silvermans(project1["Average Marginal Logistic"]), 2.5*Silvermans(project4["Average Marginal Logistic"])]
-    dataset = [project1["Average Marginal Logistic"],project4["Average Marginal Logistic"]]
+    bandwidths = [2.5*Silvermans(project1[field]), 2.5*Silvermans(project4[field])]
+    dataset = [project1[field],project4[field]]
     labels = ['Project 1', 'Project 4']
     linetypes = ['-','--']
     r = []
@@ -42,18 +43,25 @@ def CreateCompareKDEPlots():
     # plt.xlim(0,1)
     ax = plt.axes()
     ax.set_ylabel('Density')
-    ax.set_xlabel(r'Average Change in Probability of Failure ($p_i(q_j,s_i)$)')
+    if discrete:
+        ax.set_xlabel(r'Average Change in Probability of Failure')
+    else:
+        ax.set_xlabel(r'Average Change in Probability of Failure ($p_i(q_j,s_i)$)')   
     for index, result in enumerate(r):
         xinterval, yprojections = result
         ax.plot(xinterval, yprojections, label=labels[index], color='black', linestyle=linetypes[index])
-    plt.vlines(project1["Average Marginal Logistic"].mean(), 0, 3, color='black', linestyles='-')
-    plt.vlines(project4["Average Marginal Logistic"].mean(), 0, 3, color='black', linestyles='--')
+    plt.vlines(project1[field].mean(), 0, 3, color='black', linestyles='-')
+    plt.vlines(project4[field].mean(), 0, 3, color='black', linestyles='--')
     ax.legend()
-    plt.savefig('KDEChangeCompareCurve.pdf')
+    if discrete:
+        plt.savefig('KDEChangeCompareCurveDiscrete.pdf')
+    else:
+        plt.savefig('KDEChangeCompareCurve.pdf')
 
-def ProjectKDEPlot():
+def ProjectKDEPlot(discrete=False):
+    field = "Average Discrete Marginal Logistic" if discrete else "Average Marginal Logistic"
     data = pd.read_csv("StudentsProject1.csv")
-    subdata = data["Average Marginal Logistic"]
+    subdata = data[field]
     bandwidths = [2.5*Silvermans(subdata),]
     dataset = [subdata,]
     labels = ['Project 1', ]
@@ -64,18 +72,26 @@ def ProjectKDEPlot():
     plt.figure()
     ax = plt.axes()
     ax.set_ylabel('Density')
-    ax.set_xlabel(r'Average Change in Probability of Failure ($p_i(q_j,s_i)$)')
+    if discrete:
+        ax.set_xlabel(r'Average Change in Probability of Failure')
+    else:
+        ax.set_xlabel(r'Average Change in Probability of Failure ($p_i(q_j,s_i)$)')
     for index, result in enumerate(r):
         xinterval, yprojections = result
         ax.plot(xinterval, yprojections, label=labels[index], color='black', linestyle=linetypes[index])
     plt.vlines(subdata.mean(), 0, 4.5, color='black', linestyles='-')
     # ax.legend()
-    plt.savefig('KDEChangeProject1Curve.pdf')
+    if discrete:
+        plt.savefig('KDEChangeProject1CurveDiscrete.pdf')
+    else:
+        plt.savefig('KDEChangeProject1Curve.pdf')
 
 
 def main():
     CreateCompareKDEPlots()
     ProjectKDEPlot()
+    CreateCompareKDEPlots(True)
+    ProjectKDEPlot(True)
 
 if __name__ == '__main__':
     main()
